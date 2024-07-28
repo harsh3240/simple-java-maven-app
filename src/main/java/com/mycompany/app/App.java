ec3f296bb6facac94ea238f47cfb511d88de8ed8
harsh3240 <kharshdeep863@gmail.com>
2024-07-28 01:53:52 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ public class App {
 
     private static final String MESSAGE = "Hello World!";
 
-    public App() {   }
+    public App() {  }
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
